77f2fbdd3a5a90b795f763431a5149f8d825209e
Jon TURNEY <jon.turney@dronecode.org.uk>
2009-06-19 21:14:37 +0100

    Hint handling fixes
    Respect the systems ownership of the clipping region
    
    When the style changes, adjust the window size so the client area remains the same.
    Otherwise the window size may change when sizing is reflected from
    Windows to X, and some windows are drawn expecting them to be exactly the reqeusted size
    (e.g. gmplayer control window)
    
    Use the current style in determining the window border when validating which sizing
    XXX: Needs to allow for different border thickness when uncaptioned?
    
    Signed-off-by: Jon TURNEY <jon.turney@dronecode.org.uk>

--- hw/xwin/winmultiwindowwndproc.c
+++ hw/xwin/winmultiwindowwndproc.c
@@ -210,6 +210,7 @@ ValidateSizing (HWND hwnd, WindowPtr pWin,
   WinXSizeHints sizeHints;
   RECT *rect;
   int iWidth, iHeight;
+  unsigned long rcStyle;
 
   /* Invalid input checking */
   if (pWin==NULL || lParam==0)
@@ -231,19 +232,31 @@ ValidateSizing (HWND hwnd, WindowPtr pWin,
   iWidth = rect->right - rect->left;
   iHeight = rect->bottom - rect->top;
 
-  /* Now remove size of any borders */
-  iWidth -= 2 * GetSystemMetrics(SM_CXSIZEFRAME);
-  iHeight -= (GetSystemMetrics(SM_CYCAPTION)
-	      + 2 * GetSystemMetrics(SM_CYSIZEFRAME));
-	      
+  /* Now remove size of any borders and title bar */
+  rcStyle = GetWindowLongPtr(hwnd, GWL_STYLE);
+  if (rcStyle & WS_CAPTION)
+    {
+      iHeight -= GetSystemMetrics(SM_CYCAPTION);
+    }
+  if (rcStyle & WS_SIZEBOX)
+    {
+      iWidth -= 2 * GetSystemMetrics(SM_CXSIZEFRAME);
+      iHeight -= 2 * GetSystemMetrics(SM_CYSIZEFRAME);
+    }
 
   /* Constrain the size to legal values */
   ConstrainSize (sizeHints, &iWidth, &iHeight);
 
-  /* Add back the borders */
-  iWidth += 2 * GetSystemMetrics(SM_CXSIZEFRAME);
-  iHeight += (GetSystemMetrics(SM_CYCAPTION)
-	      + 2 * GetSystemMetrics(SM_CYSIZEFRAME));
+  /* Add back the size of borders and title bar */
+  if (rcStyle & WS_CAPTION)
+    {
+      iHeight += GetSystemMetrics(SM_CYCAPTION);
+    }
+  if (rcStyle & WS_SIZEBOX)
+    {
+      iWidth += 2 * GetSystemMetrics(SM_CXSIZEFRAME);
+      iHeight += 2 * GetSystemMetrics(SM_CYSIZEFRAME);
+    }
 
   /* Adjust size according to where we're dragging from */
   switch(wParam) {
@@ -990,6 +1003,17 @@ winTopLevelWindowProc (HWND hwnd, UINT message,
       winAdjustXWindow (pWin, hwnd);
       if (wParam == SIZE_MINIMIZED) winReorderWindowsMultiWindow();
       return 0; /* end of WM_SIZE handler */
+
+    case WM_STYLECHANGED:
+      /* when the style changes, adjust the window size so the client area remains the same */
+      {
+	LONG x,y;
+	DrawablePtr pDraw = &pWin->drawable;
+	x =  pDraw->x - wBorderWidth(pWin);
+	y = pDraw->y - wBorderWidth(pWin);
+	winPositionWindowMultiWindow(pWin, x, y);
+      }
+      return 0;
 
     case WM_MOUSEACTIVATE:
 
