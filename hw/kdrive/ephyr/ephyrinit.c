436f65cccb4d5fd983034c15c92085a8b59aba53
Jon TURNEY <jon.turney@dronecode.org.uk>
2009-06-19 21:14:50 +0100

    Fix various DDXs to build if DDXBEFORERESET, DDXOSFATALERROR are defined
    Provide in dmx, zephyr, fake and sdl DDXs
    ddxBeforeReset() if DDXBEFORERESET is defined
    OsVendorFatalError() if DDXOSFATALERROR is defined
    
    Signed-off-by: Jon TURNEY <jon.turney@dronecode.org.uk>

--- hw/kdrive/ephyr/ephyrinit.c
+++ hw/kdrive/ephyr/ephyrinit.c
@@ -250,6 +250,13 @@ ddxProcessArgument (int argc, char **argv, int i)
   return KdProcessArgument (argc, argv, i);
 }
 
+#ifdef DDXBEFORERESET
+void
+ddxBeforeReset (void)
+{
+}
+#endif
+
 void
 OsVendorInit (void)
 {
@@ -263,6 +270,13 @@ OsVendorInit (void)
 
   KdOsInit (&EphyrOsFuncs);
 }
+
+#ifdef DDXOSFATALERROR
+void
+OsVendorFatalError(void)
+{
+}
+#endif
 
 /* 'Fake' cursor stuff, could be improved */
 
