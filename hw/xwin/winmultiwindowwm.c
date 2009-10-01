b6b49ad0279bb9dfe6ef9ed4c0cb98c7f07c5f71
Jon TURNEY <jon.turney@dronecode.org.uk>
2009-10-01 13:12:48 +0100

    Cygwin/X: Improve choice of display name used by internal clients
    Choose display name used to connect internal clients and exported into
    environment of processes started by traymenu so that it uses a transport
    we know is working
    
    This should mean the server can start correctly with -multiwindow
    and/or -clipboard and any two of -nolisten inet6 -nolisten inet
    and -nolisten unix (the server will correctly refuse to start if all 3
    are used, as it must be listening on at least one socket)
    
    Signed-off-by: Jon TURNEY <jon.turney@dronecode.org.uk>

--- hw/xwin/winmultiwindowwm.c
+++ hw/xwin/winmultiwindowwm.c
@@ -136,7 +136,6 @@ typedef struct _XMsgProcArgRec {
  * References to external symbols
  */
 
-extern char *display;
 extern void ErrorF (const char* /*f*/, ...);
 
 /*
@@ -926,8 +925,7 @@ winMultiWindowXMsgProc (void *pArg)
   XSetIOErrorHandler (winMultiWindowXMsgProcIOErrorHandler);
 
   /* Setup the display connection string x */
-  snprintf (pszDisplay,
-	    512, "127.0.0.1:%s.%d", display, (int)pProcArg->dwScreen);
+  winGetDisplayName(pszDisplay, (int)pProcArg->dwScreen);
 
   /* Print the display connection string */
   ErrorF ("winMultiWindowXMsgProc - DISPLAY=%s\n", pszDisplay);
@@ -1310,11 +1308,7 @@ winInitMultiWindowWM (WMInfoPtr pWMInfo, WMProcArgPtr pProcArg)
   XSetIOErrorHandler (winMultiWindowWMIOErrorHandler);
 
   /* Setup the display connection string x */
-  snprintf (pszDisplay,
-	    512,
-	    "127.0.0.1:%s.%d",
-	    display,
-	    (int) pProcArg->dwScreen);
+  winGetDisplayName(pszDisplay, (int)pProcArg->dwScreen);
 
   /* Print the display connection string */
   ErrorF ("winInitMultiWindowWM - DISPLAY=%s\n", pszDisplay);
