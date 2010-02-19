ef66a7e19f5d0425a3e978f25413d957e105c0f2
Colin Harrison <colin.harrison@virgin.net>
2010-02-18 22:25:18 -0600

    Xming: Fix the annoying menu/tooltip punch-through problem in -multiwindow mode.
    http://sourceware.org/bugzilla/show_bug.cgi?id=9929
    
    Copyright (C) Colin Harrison 2005-2008
    http://www.straightrunning.com/XmingNotes/
    http://sourceforge.net/projects/xming/
    
    Signed-off-by: Yaakov Selkowitz <yselkowitz@users.sourceforge.net>

--- hw/xwin/winmultiwindowwndproc.c
+++ hw/xwin/winmultiwindowwndproc.c
@@ -1020,6 +1020,7 @@ winTopLevelWindowProc (HWND hwnd, UINT message,
 #endif
       /* Adjust the X Window to the moved Windows window */
       winAdjustXWindow (pWin, hwnd);
+      if (wParam == SIZE_MINIMIZED) winReorderWindowsMultiWindow();
       return 0; /* end of WM_SIZE handler */
 
     case WM_STYLECHANGING:
