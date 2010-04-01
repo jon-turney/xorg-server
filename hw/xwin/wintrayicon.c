2ff4132f32ac9927d9bce28763369fdc3309ae4c
Jon TURNEY <jon.turney@dronecode.org.uk>
2010-04-01 15:08:26 +0100

    Bring the X window to the front on a single left click on the tray icon, like the comment says we do

--- hw/xwin/wintrayicon.c
+++ hw/xwin/wintrayicon.c
@@ -121,7 +121,7 @@ winHandleIconMessage (HWND hwnd, UINT message,
     {
     case WM_LBUTTONUP:
       /* Restack and bring all windows to top */
-      SetForegroundWindow (hwnd);
+      SetForegroundWindow (pScreenPriv->hwndScreen);
 
 #ifdef XWIN_MULTIWINDOWEXTWM
       if (pScreenInfo->fMWExtWM)
