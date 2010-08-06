66f3680cb47fbd091924f8ca194085a740f59b2f
Yaakov Selkowitz <yselkowitz@users.sourceforge.net>
2010-08-06 00:29:27 -0500

    Cygwin/X: Fix typo in keyboard layout names
    Signed-off-by: Yaakov Selkowitz <yselkowitz@users.sourceforge.net>

--- hw/xwin/winlayouts.h
+++ hw/xwin/winlayouts.h
@@ -56,7 +56,7 @@ WinKBLayoutRec winKBLayouts[] =
     {0x20409, -1, "pc105", "us_intl", NULL, NULL, "English (USA, International)"}, 
     {  0x809, -1, "pc105", "gb",      NULL, NULL, "English (United Kingdom)"},
     { 0x1009, -1, "pc105", "ca",      "fr", NULL, "French (Canada)"},
-    {0x11009, -1, "pc105", "ca",      "multix", NULL, "Candian Multilingual Standard"},
+    {0x11009, -1, "pc105", "ca",      "multix", NULL, "Canadian Multilingual Standard"},
     { 0x1809, -1, "pc105", "ie",      NULL, NULL, "Irish"},
     {  0x40a, -1, "pc105", "es",      NULL, NULL, "Spanish (Spain, Traditional Sort)"},
     {  0x80a, -1, "pc105", "latam",   NULL, NULL, "Latin American"},
