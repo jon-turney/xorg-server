6f661c5f1cf2259c630a305a86cb267d446fe1ba
Jon TURNEY <jon.turney@dronecode.org.uk>
2010-09-09 19:41:09 +0100

    glx: Flush context which is being made non-current due to drawable going away
    indirect rendering glean tests fail with GLXBadCurrentWindow
    e.g. glean -t 'fpexceptions getString'

--- glx/glxcmds.c
+++ glx/glxcmds.c
@@ -213,6 +213,13 @@ static __GLXcontext *__glXdirectContextCreate(__GLXscreen *screen,
     return context;
 }
 
+void
+FlushContext(__GLXcontext *cx)
+{
+  CALL_Flush( GET_DISPATCH(), () );
+  cx->hasUnflushedCommands = GL_FALSE;
+}
+
 /**
  * Create a GL context with the given properties.  This routine is used
  * to implement \c glXCreateContext, \c glXCreateNewContext, and
