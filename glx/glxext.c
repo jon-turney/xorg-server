654498e89bb125a935d1fa1edda7dec876ef24eb
Jon TURNEY <jon.turney@dronecode.org.uk>
2010-09-24 16:57:19 +0100

    glx: In DrawableGone, only clear context tags when we are also deleting the context
    In DrawableGone, don't clear tags for context which has idExists set
    The client hasn't deleted them yet and may still refer to the tag
    
    https://bugs.freedesktop.org/show_bug.cgi?id=30089
    
    Signed-off-by: Jon TURNEY <jon.turney@dronecode.org.uk>

--- glx/glxext.c
+++ glx/glxext.c
@@ -147,7 +147,8 @@ static Bool DrawableGone(__GLXdrawable *glxPriv, XID xid)
 	    if (c == __glXLastContext)
 		__glXFlushContextCache();
 
-	    for (i = 1; i < currentMaxClients; i++) {
+	    if (!c->idExists) {
+              for (i = 1; i < currentMaxClients; i++) {
 		if (clients[i]) {
 		    __GLXclientState *cl = glxGetClient(clients[i]);
 
@@ -160,6 +161,7 @@ static Bool DrawableGone(__GLXdrawable *glxPriv, XID xid)
 			}
 		    }
 		}
+              }
 	    }
 	}
 	if (c->drawPriv == glxPriv)
