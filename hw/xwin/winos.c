b40f66090c1ddffb3201f2bdc32f757907b51750
Jon Turney <jon.turney@dronecode.org.uk>
2011-08-31 20:57:05 +0100

    OS version detection and reporting
    Add OS version detection and reporting
    Also report if OS is WoW64 or not when reporting OS version
    
    v2: Fix version reporting on cygwin64
    
    DWORD is a long int on 32-bit, but just an int on 64-bit, so cast to (int) and
    use %d to output
    Checking for Wow32 isn't interesting on Win64
    
    v3: Add the names for Windows 8.1, Windows Server 2012 R2
    
    Add marketing names for Windows 6.3 versions
    Manifest for compatbility with those versions, so we don't get lied to by
    GetVersionEx()
    Also, make description in manifest a bit more generic
    Also, Add marketing name for Windows 5.2 workstation
    
    v4:
    Cosmetic fix to version reporting on x86_64
    Warning fixes
    
    winprocarg.c:1214:22: warning: assignment discards ‘const’ qualifier from pointer target type [enabled by default]
    etc.
    
    v5:
    Add marketing name for Windows 6.4 and manifest for compatibility
    
    v6:
    Add marketing names for Windows 10.0
    
    In build 9879, Windows 10 Technical Preview reports a version number of 6.4
    In build 9926, Windows 10 Technical Preview reports a version number of 10.0
    
    Handle both cases for the moment.
    
    v7:
    Add Windows 11, ARM64 reporting

--- hw/xwin/winos.c
+++ hw/xwin/winos.c
@@ -31,26 +31,82 @@
 #endif
 #include "win.h"
 
-typedef BOOL (WINAPI *LPFN_ISWOW64PROCESS)(HANDLE, PBOOL);
-
-static const char*
-IsWow64(void)
+static USHORT WowNativeMachine (void)
 {
-#ifdef __x86_64__
-    return " (64-bit)";
-#else
-    WINBOOL bIsWow64;
-    LPFN_ISWOW64PROCESS fnIsWow64Process =
-        (LPFN_ISWOW64PROCESS) GetProcAddress(GetModuleHandle(TEXT("kernel32")),
-                                             "IsWow64Process");
-    if (NULL != fnIsWow64Process) {
-        if (fnIsWow64Process(GetCurrentProcess(), &bIsWow64))
-            return bIsWow64 ? " (WoW64)" : " (32-bit)";
-    }
+  typedef WINBOOL (WINAPI *PFNISWOW64PROCESS2)(HANDLE, USHORT *, USHORT *);
+  PFNISWOW64PROCESS2 pfnIsWow64Process2 = (PFNISWOW64PROCESS2)GetProcAddress(GetModuleHandle("kernel32"), "IsWow64Process2");
 
-    /* OS doesn't support IsWow64Process() */
-    return "";
+  typedef WINBOOL (WINAPI *PFNISWOW64PROCESS)(HANDLE, PBOOL);
+  PFNISWOW64PROCESS pfnIsWow64Process = (PFNISWOW64PROCESS)GetProcAddress(GetModuleHandle("kernel32"), "IsWow64Process");
+
+  USHORT processMachine, nativeMachine;
+  if ((pfnIsWow64Process2) &&
+      (pfnIsWow64Process2(GetCurrentProcess(), &processMachine, &nativeMachine)))
+    return nativeMachine;
+  else if (pfnIsWow64Process) {
+#ifdef _X86_
+    WINBOOL bIsWow64 = FALSE;
+    if (pfnIsWow64Process(GetCurrentProcess(), &bIsWow64))
+      return bIsWow64 ? IMAGE_FILE_MACHINE_AMD64 : IMAGE_FILE_MACHINE_I386;
 #endif
+  }
+
+#ifdef __x86_64__
+  return IMAGE_FILE_MACHINE_AMD64;
+#else
+  return IMAGE_FILE_MACHINE_I386;
+#endif
+}
+
+static char *machine_name(USHORT machine)
+{
+  const char *result;
+
+  switch (machine)
+      {
+      case IMAGE_FILE_MACHINE_I386:
+          result = "x86";
+          break;
+      case IMAGE_FILE_MACHINE_AMD64:
+          result = "x64";
+          break;
+      case IMAGE_FILE_MACHINE_ARM64:
+          result = "ARM64";
+          break;
+      default:
+          {
+              static char buf[8];
+              snprintf(buf, sizeof(buf), "%x", machine);
+              result = buf;
+          }
+      }
+  return strdup(result);
+}
+
+static const char* arch_wow(void)
+{
+  static char arch[32];
+
+#ifdef __x86_64__
+  USHORT processMachine = IMAGE_FILE_MACHINE_AMD64;
+#else
+  USHORT processMachine = IMAGE_FILE_MACHINE_I386;
+#endif
+
+  USHORT nativeMachine = WowNativeMachine();
+
+  char *processMachineName = machine_name(processMachine);
+  char *nativeMachineName = machine_name(nativeMachine);
+
+  if (processMachine != nativeMachine)
+      sprintf(arch, "%s-on-%s", processMachineName, nativeMachineName);
+  else
+      sprintf(arch, "%s", processMachineName);
+
+  free(nativeMachineName);
+  free(processMachineName);
+
+  return arch;
 }
 
 /*
@@ -61,12 +117,96 @@ void
 winOS(void)
 {
     OSVERSIONINFOEX osvi = {0};
+    const char *windowstype = "Unknown";
+    const char *prodName = "Unknown";
 
     /* Get operating system version information */
     osvi.dwOSVersionInfoSize = sizeof(osvi);
     GetVersionEx((LPOSVERSIONINFO)&osvi);
 
-    ErrorF("OS: Windows NT %d.%d build %d%s\n",
-           (int)osvi.dwMajorVersion, (int)osvi.dwMinorVersion,
-           (int)osvi.dwBuildNumber, IsWow64());
+    /* Branch on platform ID */
+    switch (osvi.dwPlatformId) {
+    case VER_PLATFORM_WIN32_NT:
+        windowstype = "Windows NT";
+
+        if (osvi.dwMajorVersion <= 4)
+            prodName = "Windows NT";
+        else if (osvi.dwMajorVersion == 10) {
+            if (osvi.dwMinorVersion == 0) {
+                if (osvi.dwMinorVersion < 22000)
+                    {
+                        if (osvi.wProductType == VER_NT_WORKSTATION)
+                            prodName = "Windows 10";
+                        else
+                            prodName = "Windows 10 Server";
+                    }
+                else
+                    {
+                        if (osvi.wProductType == VER_NT_WORKSTATION)
+                            prodName = "Windows 11";
+                        else
+                            prodName = "Windows 11 Server";
+                    }
+            }
+        }
+        else if (osvi.dwMajorVersion == 6) {
+            if (osvi.dwMinorVersion == 4) {
+                if (osvi.wProductType == VER_NT_WORKSTATION)
+                    prodName = "Windows 10";
+                else
+                    prodName = "Windows Server 2016";
+            }
+            if (osvi.dwMinorVersion == 3) {
+                if (osvi.wProductType == VER_NT_WORKSTATION)
+                    prodName = "Windows 8.1";
+                else
+                    prodName = "Windows Server 2012 R2";
+            }
+            if (osvi.dwMinorVersion == 2) {
+                if (osvi.wProductType == VER_NT_WORKSTATION)
+                    prodName = "Windows 8";
+                else
+                    prodName = "Windows Server 2012";
+            }
+            else if (osvi.dwMinorVersion == 1) {
+                if (osvi.wProductType == VER_NT_WORKSTATION)
+                    prodName = "Windows 7";
+                else
+                    prodName = "Windows Server 2008 R2";
+            }
+            else if (osvi.dwMinorVersion == 0) {
+                if (osvi.wProductType == VER_NT_WORKSTATION)
+                    prodName = "Windows Vista";
+                else
+                    prodName = "Windows Server 2008";
+            }
+        }
+        else if (osvi.dwMajorVersion == 5) {
+            if (osvi.dwMinorVersion == 2) {
+                if (osvi.wProductType == VER_NT_WORKSTATION)
+                    prodName = "Windows XP x64 Edition";
+                else if (GetSystemMetrics(SM_SERVERR2))
+                    prodName = "Windows Server 2003 R2";
+                else
+                    prodName = "Windows Server 2003";
+            }
+            else if (osvi.dwMinorVersion == 1)
+                prodName = "Windows XP";
+            else if (osvi.dwMinorVersion == 0) {
+                prodName = "Windows 2000";
+                break;
+            }
+        }
+
+        break;
+
+    case VER_PLATFORM_WIN32_WINDOWS:
+        windowstype = "Windows";
+        break;
+    }
+
+    ErrorF("OS: %s %s [%s %d.%d build %d] %s\n",
+           prodName, osvi.szCSDVersion,
+           windowstype, (int)osvi.dwMajorVersion, (int)osvi.dwMinorVersion,
+           (int)osvi.dwBuildNumber, arch_wow());
 }
