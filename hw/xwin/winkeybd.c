1de42a64916bcae7d40b7212d46fbf7a1c715a4a
Jon TURNEY <jon.turney@dronecode.org.uk>
2011-09-03 12:48:50 +0100

    hw/xwin: Add XF86 xkb private actions for grab debugging to XWin
    In order to invoke these actions, you will need to add them to you
    XKB maps, as described in [1]
    
    http://lists.x.org/archives/xorg-devel/2011-June/023508.html

--- hw/xwin/winkeybd.c
+++ hw/xwin/winkeybd.c
@@ -41,6 +41,7 @@
 #include "winmsg.h"
 
 #include "xkbsrv.h"
+#include "dixgrabs.h"
 
 static Bool g_winKeyState[NUM_KEYCODES];
 
@@ -521,4 +522,35 @@ void winFixShiftKeys (int iScanCode)
     winSendKeyEvent (KEY_ShiftR, FALSE);
   if (iScanCode == KEY_ShiftR && g_winKeyState[KEY_ShiftL])
     winSendKeyEvent (KEY_ShiftL, FALSE);
+}
+
+/*
+ */
+int
+XkbDDXPrivate(DeviceIntPtr dev,KeyCode key,XkbAction *act)
+{
+    XkbAnyAction *xf86act = &(act->any);
+    char msgbuf[XkbAnyActionDataSize+1];
+
+    if (xf86act->type == XkbSA_XFree86Private)
+      {
+        memcpy(msgbuf, xf86act->data, XkbAnyActionDataSize);
+        msgbuf[XkbAnyActionDataSize]= '\0';
+        if (strcasecmp(msgbuf, "prgrbs")==0) {
+            DeviceIntPtr tmp;
+            winMsg(X_INFO, "Printing all currently active device grabs:\n");
+            for (tmp = inputInfo.devices; tmp; tmp = tmp->next)
+                if (tmp->deviceGrab.grab)
+                    PrintDeviceGrabInfo(tmp);
+            winMsg(X_INFO, "End list of active device grabs\n");
+        }
+        else if (strcasecmp(msgbuf, "ungrab")==0)
+            UngrabAllDevices(FALSE);
+        else if (strcasecmp(msgbuf, "clsgrb")==0)
+            UngrabAllDevices(TRUE);
+        else if (strcasecmp(msgbuf, "prwins")==0)
+            PrintWindowTree();
+    }
+
+    return 0;
 }
