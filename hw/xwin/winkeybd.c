be44001937612e56fb644087d5415bbc44e6f01b
Jon TURNEY <jon.turney@dronecode.org.uk>
2011-09-03 12:48:50 +0100

    hw/xwin: Add XF86 xkb private actions for grab debugging to XWin
    Add four new private XKB actions for debugging:
    
    ctrl-alt-keypad /  Ungrab  Ungrab all active grabs
    ctrl-alt-keypad *  ClsGrb  Kill clients with active grabs
                       PrGrbs  Print grab info to log
                       PrWins  Dump window tree to log file.
    
    For security reasons, since xkeyboard-config 2.5, the Ungrab and ClsGrb actions
    must be explicitly enabled using '-xkboption grab:break_actions'
    
    In order to invoke the PrGrbs and PrWins actions, they need to be added to the XKB
    map, see [1]
    
    [1] http://lists.x.org/archives/xorg-devel/2011-June/023508.html

--- hw/xwin/winkeybd.c
+++ hw/xwin/winkeybd.c
@@ -40,6 +40,7 @@
 #include "winmsg.h"
 
 #include "xkbsrv.h"
+#include "dixgrabs.h"
 
 /* C does not have a logical XOR operator, so we use a macro instead */
 #define LOGICAL_XOR(a,b) ((!(a) && (b)) || ((a) && !(b)))
@@ -522,4 +523,35 @@ winFixShiftKeys(int iScanCode)
         winSendKeyEvent(KEY_ShiftR, FALSE);
     if (iScanCode == KEY_ShiftR && g_winKeyState[KEY_ShiftL])
         winSendKeyEvent(KEY_ShiftL, FALSE);
+}
+
+/*
+ */
+int
+XkbDDXPrivate(DeviceIntPtr dev,KeyCode key,XkbAction *act)
+{
+    XkbAnyAction *xf86act = &(act->any);
+    char msgbuf[XkbAnyActionDataSize+1];
+
+    if (xf86act->type == XkbSA_XFree86Private)
+      {
+        memcpy(msgbuf, xf86act->data, XkbAnyActionDataSize);
+        msgbuf[XkbAnyActionDataSize]= '\0';
+        if (strcasecmp(msgbuf, "prgrbs")==0) {
+            DeviceIntPtr tmp;
+            winMsg(X_INFO, "Printing all currently active device grabs:\n");
+            for (tmp = inputInfo.devices; tmp; tmp = tmp->next)
+                if (tmp->deviceGrab.grab)
+                    PrintDeviceGrabInfo(tmp);
+            winMsg(X_INFO, "End list of active device grabs\n");
+        }
+        else if (strcasecmp(msgbuf, "ungrab")==0)
+            UngrabAllDevices(FALSE);
+        else if (strcasecmp(msgbuf, "clsgrb")==0)
+            UngrabAllDevices(TRUE);
+        else if (strcasecmp(msgbuf, "prwins")==0)
+            PrintWindowTree();
+    }
+
+    return 0;
 }
