f063b6a6b5ea5b2d19c01e080ea737e68681110d
Ryan Pavlik <rpavlik@iastate.edu>
2011-11-04 14:49:15 -0500

    os/xsha1.c: Preemptively include <X11/Xwindows.h> on Win32.
    Avoids duplicate defines if someone includes windows.h before
    the specially wrapped X11/Xwindows.h header.

--- os/xsha1.c
+++ os/xsha1.c
@@ -64,6 +64,13 @@ int x_sha1_final(void *ctx, unsigned char result[20])
 
 #elif defined(HAVE_SHA1_IN_LIBGCRYPT) /* Use libgcrypt for SHA1 */
 
+# if defined(WIN32) && !defined(__CYGWIN__)
+/*
+ * Must be included before any other header includes windows.h, to avoid
+ * conflicting definitions.
+ */
+#  include <X11/Xwindows.h>
+# endif
 # include <gcrypt.h>
 
 void *x_sha1_init(void)
