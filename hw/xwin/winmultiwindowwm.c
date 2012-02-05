4b427e3f64ec41eac0c5696e7964813dea307e08
Jon TURNEY <jon.turney@dronecode.org.uk>
2012-02-05 11:24:54 +0000

    Improve multiwindow internal WM PropertyNotify debug output
    Improve multiwindow internal WM PropertyNotify debug output, so it reports the
    name of the window property which has changed
    
    Signed-off-by: Jon TURNEY <jon.turney@dronecode.org.uk>

--- hw/xwin/winmultiwindowwm.c
+++ hw/xwin/winmultiwindowwm.c
@@ -1209,6 +1209,10 @@ winMultiWindowXMsgProc(void *pArg)
             }
         }
         else if (event.type == PropertyNotify) {
+            char *atomName =
+                XGetAtomName(pProcArg->pDisplay, event.xproperty.atom);
+            winDebug("winMultiWindowXMsgProc: PropertyNotify %s\n", atomName);
+            XFree(atomName);
             if (event.xproperty.atom == atmWmName) {
                 memset(&msg, 0, sizeof(msg));
 
