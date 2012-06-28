8c366bee4c36e2d66db137707427c5c183b85282
Jon TURNEY <jon.turney@dronecode.org.uk>
2012-06-28 13:28:50 +0100

    Add -notrapsignals
    Add -notrapsignals to disable catching of fatal signals (to either
    continue or log) and just use the default signal processing instead
    Log the thread id where the signal is delivered
    Various cosmetic fixes to fatal error logging
    
    Signed-off-by: Jon TURNEY <jon.turney@dronecode.org.uk>

--- os/utils.c
+++ os/utils.c
@@ -122,6 +122,8 @@ __stdcall unsigned long GetTickCount(void);
 
 #include "picture.h"
 
+Bool install_os_signal_handler = TRUE;
+
 Bool noTestExtensions;
 
 #ifdef COMPOSITE
@@ -522,6 +524,7 @@ UseMsg(void)
 #ifdef RLIMIT_STACK
     ErrorF("-ls int                limit stack space to N Kb\n");
 #endif
+    ErrorF("-notrapsignals         disable catching of fatal signals\n");
 #ifdef LOCK_SERVER
     ErrorF("-nolock                disable the locking mechanism\n");
 #endif
@@ -767,6 +770,9 @@ ProcessCommandLine(int argc, char *argv[])
                 UseMsg();
         }
 #endif
+        else if (strcmp(argv[i], "-notrapsignals") == 0) {
+            install_os_signal_handler = FALSE;
+        }
 #ifdef LOCK_SERVER
         else if (strcmp(argv[i], "-nolock") == 0) {
 #if !defined(WIN32) && !defined(__CYGWIN__)
