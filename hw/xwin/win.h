e2d0543c20cc5ecf367653b431954e1d3bb6fde4
Jon Turney <jon.turney@dronecode.org.uk>
2012-09-02 15:51:52 +0100

    Make AIGLX work in non-toplevel windows
    When needed, create a native child windows corresponding to the X window
    hierarchy, so OpenGL drawing will take place into an appropriately placed and
    sized native window.
    
    Provide a new window class and wndproc for these child windows (it appears they
    can't use the same one as toplevel windows for some reason)
    
    Use WS_DISABLED style for these child windows, so they never gain input focus.
    Use WS_EX_TRANSPARENT, so they don't obscure non-OpenGL drawing
    
    Add isTopLevelWindow helper to identify when we are working on one of these
    child windows
    
    When X maps, moves or reparents the child window, update the native window
    appropriately
    
    Correctly handle GLX windows being reparented from a mapped window to an
    unmapped window
    
    Clean up HWNDs which are implicitly deleted when the parent window is deleted
    
    v2:
    Add needed cast when storing WIN_WID_PROP XID as a HANDLE-type Window property
    
    v3:
    Update for pointer -> void * type change
    
    Stop recursion in winCreateWindowsWindowHierarchy() at the root window to fix a
    crash which occurs when trying to use OpenGL on the root window in multiwindow
    mode.  (OpenGL drawing still doesn't do anything because the root window is
    hidden, but now it doesn't crash)
    
    Testcase: ogl.c from fd.o bug #8443
    
    v4:
    Use %p printf format for HWND and WindowPtr to fix printf format warnings
    Don't bother storing unused HDC returned by BeginPaint()
    
    Signed-off-by: Jon Turney <jon.turney@dronecode.org.uk>

--- hw/xwin/win.h
+++ hw/xwin/win.h
@@ -986,6 +986,8 @@ int
 
 LRESULT CALLBACK
 winTopLevelWindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK
+winChildWindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
 
 /*
  * wintrayicon.c
