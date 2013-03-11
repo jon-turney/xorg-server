0264f4f6e358e3c6d988e425f6586750749085c1
Jon TURNEY <jon.turney@dronecode.org.uk>
2013-03-11 14:34:32 +0000

    Handle -displayfd and an explicit display number sensibly
    Handle -displayfd and an explicit display number sensibly, e.g. use the
    explicitly specified display number, and write it to the displayfd

--- include/opaque.h
+++ include/opaque.h
@@ -51,6 +51,7 @@ extern _X_EXPORT int defaultScreenSaverBlanking;
 extern _X_EXPORT int defaultScreenSaverAllowExposures;
 extern _X_EXPORT char *display;
 extern _X_EXPORT int displayfd;
+extern _X_EXPORT Bool explicit_display;
 
 extern _X_EXPORT int defaultBackingStore;
 extern _X_EXPORT Bool disableBackingStore;
--- os/utils.c
+++ os/utils.c
@@ -596,6 +596,7 @@ ProcessCommandLine(int argc, char *argv[])
         else if (argv[i][0] == ':') {
             /* initialize display */
             display = argv[i];
+            explicit_display = TRUE;
             display++;
             if (!VerifyDisplayName(display)) {
                 ErrorF("Bad display name: %s\n", display);
@@ -666,7 +667,6 @@ ProcessCommandLine(int argc, char *argv[])
         else if (strcmp(argv[i], "-displayfd") == 0) {
             if (++i < argc) {
                 displayfd = atoi(argv[i]);
-                display = NULL;
                 nolock = TRUE;
             }
             else
