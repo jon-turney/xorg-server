ce53ef8fba15ff8a8236ba877d756458276d45b4
Jon TURNEY <jon.turney@dronecode.org.uk>
2013-04-06 14:59:09 +0100

    Add ability to annotate symbols so they are not processes by sdksyms
    SDKSYMS is defined when generating sdksyms
    XXX: It's sometimes asserted that X_EXPORT does this but I don't see the mechanism

--- hw/xfree86/sdksyms.sh
+++ hw/xfree86/sdksyms.sh
@@ -311,8 +311,8 @@ topdir=$1
 shift
 LC_ALL=C
 export LC_ALL
-${CPP:-cpp} "$@" sdksyms.c > /dev/null || exit $?
-${CPP:-cpp} "$@" sdksyms.c | ${AWK:-awk} -v topdir=$topdir '
+${CPP:-cpp} "$@" -DSDKSYMS sdksyms.c > /dev/null || exit $?
+${CPP:-cpp} "$@" -DSDKSYMS sdksyms.c | ${AWK:-awk} -v topdir=$topdir '
 BEGIN {
     sdk = 0;
     print("/*");
