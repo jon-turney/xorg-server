9bb73303da4c54715ad844834397056cdbd75b86
Jon Turney <jon.turney@dronecode.org.uk>
2013-04-09 18:55:47 +0100

    Undefine _XSERVER64 in multiwindow WM
    The XEvent type suffers from the _XSERVER64 issues noted previously: certain
    members are long int in Xlib, but are made int in the server by XSERVER64.
    
    Undefine _XSERVER64 in multiwindow WM, so client code can build correctly on
    x86_64.
    
    This is needed to send the WM_KILL ClientMessage using XSendEvent() correctly.
    
    Move winUpdateWindowPosition() to winmultwindowproc.c, as it needs to access the
    server Drawable structure correctly, which uses affected types.
    
    (We still play a dangerous game with bringing the WindowPtr type into scope so
    we can use it in window shaping code to access the server's shape data.  This
    would be better rewritten as client code using ShapeNotify and
    XShapeGetRectangles())

--- hw/xwin/winmultiwindowwm.c
+++ hw/xwin/winmultiwindowwm.c
@@ -34,6 +34,16 @@
 #ifdef HAVE_XWIN_CONFIG_H
 #include <xwin-config.h>
 #endif
+
+/*
+ * Including any server header might define the macro _XSERVER64 on 64 bit machines.
+ * That macro must _NOT_ be defined for Xlib client code, otherwise bad things happen.
+ * So let's undef that macro if necessary.
+ */
+#ifdef _XSERVER64
+#undef _XSERVER64
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -58,8 +68,6 @@
 #include "winwindow.h"
 #include "winprefs.h"
 #include "window.h"
-#include "pixmapstr.h"
-#include "windowstr.h"
 #include "winglobals.h"
 #include "windisplay.h"
 #include "winmultiwindowicons.h"
@@ -80,6 +88,7 @@ extern void winDebug(const char *format, ...);
 extern void winReshapeMultiWindow(WindowPtr pWin);
 extern void winUpdateRgnMultiWindow(WindowPtr pWin);
 extern void winSetAuthorization(void);
+extern void winUpdateWindowPosition(HWND hWnd, HWND * zstyle);
 
 #ifndef CYGDEBUG
 #define CYGDEBUG NO
@@ -196,9 +205,6 @@ CheckAnotherWindowManager(Display * pDisplay, DWORD dwScreen,
 
 static void
  winApplyHints(Display * pDisplay, Window iWindow, HWND hWnd, HWND * zstyle);
-
-void
- winUpdateWindowPosition(HWND hWnd, HWND * zstyle);
 
 /*
  * Local globals
@@ -533,6 +539,7 @@ SendXMessage(Display * pDisplay, Window iWin, Atom atmType, long nData)
     XEvent e;
 
     /* Prepare the X event structure */
+    memset(&e, 0, sizeof(e));
     e.type = ClientMessage;
     e.xclient.window = iWin;
     e.xclient.message_type = atmType;
@@ -1915,58 +1922,4 @@ winApplyHints(Display * pDisplay, Window iWindow, HWND hWnd, HWND * zstyle)
     winDebug
         ("winApplyHints: iWindow 0x%08x hints 0x%08x style 0x%08x exstyle 0x%08x\n",
          iWindow, hint, style, exStyle);
-}
-
-void
-winUpdateWindowPosition(HWND hWnd, HWND * zstyle)
-{
-    int iX, iY, iWidth, iHeight;
-    int iDx, iDy;
-    RECT rcNew;
-    WindowPtr pWin = GetProp(hWnd, WIN_WINDOW_PROP);
-    DrawablePtr pDraw = NULL;
-
-    if (!pWin)
-        return;
-    pDraw = &pWin->drawable;
-    if (!pDraw)
-        return;
-
-    /* Get the X and Y location of the X window */
-    iX = pWin->drawable.x + GetSystemMetrics(SM_XVIRTUALSCREEN);
-    iY = pWin->drawable.y + GetSystemMetrics(SM_YVIRTUALSCREEN);
-
-    /* Get the height and width of the X window */
-    iWidth = pWin->drawable.width;
-    iHeight = pWin->drawable.height;
-
-    /* Setup a rectangle with the X window position and size */
-    SetRect(&rcNew, iX, iY, iX + iWidth, iY + iHeight);
-
-    winDebug("winUpdateWindowPosition - drawable extent (%d, %d)-(%d, %d)\n",
-             rcNew.left, rcNew.top, rcNew.right, rcNew.bottom);
-
-    AdjustWindowRectEx(&rcNew, GetWindowLongPtr(hWnd, GWL_STYLE), FALSE,
-                       GetWindowLongPtr(hWnd, GWL_EXSTYLE));
-
-    /* Don't allow window decoration to disappear off to top-left as a result of this adjustment */
-    if (rcNew.left < GetSystemMetrics(SM_XVIRTUALSCREEN)) {
-        iDx = GetSystemMetrics(SM_XVIRTUALSCREEN) - rcNew.left;
-        rcNew.left += iDx;
-        rcNew.right += iDx;
-    }
-
-    if (rcNew.top < GetSystemMetrics(SM_YVIRTUALSCREEN)) {
-        iDy = GetSystemMetrics(SM_YVIRTUALSCREEN) - rcNew.top;
-        rcNew.top += iDy;
-        rcNew.bottom += iDy;
-    }
-
-    winDebug("winUpdateWindowPosition - Window extent (%d, %d)-(%d, %d)\n",
-             rcNew.left, rcNew.top, rcNew.right, rcNew.bottom);
-
-    /* Position the Windows window */
-    SetWindowPos(hWnd, *zstyle, rcNew.left, rcNew.top,
-                 rcNew.right - rcNew.left, rcNew.bottom - rcNew.top, 0);
-
 }
