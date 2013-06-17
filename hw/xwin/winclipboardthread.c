2955cabedb70b154286bd494f19a68940c3d8899
Jon TURNEY <jon.turney@dronecode.org.uk>
2013-06-17 01:30:09 +0100

    Hoist setting of g_fClipboardStarted flag up one level
    Hoist the setting of g_fClipboardStarted flag up one level.
    
    Also move up the clearing of the g_fClipboardLaunched at the end of clipboard
    function.

--- hw/xwin/winclipboardthread.c
+++ hw/xwin/winclipboardthread.c
@@ -54,8 +54,6 @@ extern void winSetAuthorization(void);
  */
 
 extern Bool g_fUnicodeClipboard;
-extern Bool g_fClipboardStarted;
-extern Bool g_fClipboardLaunched;
 extern Bool g_fClipboard;
 extern HWND g_hwndClipboard;
 extern void *g_pClipboardDisplay;
@@ -267,9 +265,6 @@ winClipboardProc(void *pvNotUsed)
         pthread_exit(NULL);
     }
 
-    /* Signal that the clipboard client has started */
-    g_fClipboardStarted = TRUE;
-
     /* Loop for X events */
     while (1) {
         /* Setup the file descriptor set */
@@ -405,8 +400,6 @@ winClipboardProc(void *pvNotUsed)
 #endif
 
     /* global clipboard variable reset */
-    g_fClipboardLaunched = FALSE;
-    g_fClipboardStarted = FALSE;
     g_iClipboardWindow = None;
     g_pClipboardDisplay = NULL;
     g_hwndClipboard = NULL;
