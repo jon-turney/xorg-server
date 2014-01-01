f0c3c90c94592a3ff83974779847266bc6dc3aee
Jon TURNEY <jon.turney@dronecode.org.uk>
2014-01-01 17:51:08 +0000

    Cause the X server to terminate if the WM internal client thread exits due to an error
    We can't call FatalError from an internal client thread, so these failures
    are silent at the moment (in the sense that no pop-up occurs to tell the user
    that we are terminating or why).
    
    Taking the server down is kind of harsh, perhaps we should try to restart the WM
    internal client thread a few times, after an increasing interval.
    
    Also, I'm just noticing I should be using pthread_kill() to ensure the signal is
    delivered to the thread which is running the main dispatch loop so that will
    exit the select() it may be blocked in and notice that DE_TERMINATE is set.
    
    Signed-off-by: Jon TURNEY <jon.turney@dronecode.org.uk>

--- hw/xwin/winmultiwindowwm.c
+++ hw/xwin/winmultiwindowwm.c
@@ -169,6 +169,9 @@ static int
 static int
  winMultiWindowXMsgProcIOErrorHandler(Display * pDisplay);
 
+static void
+winMultiWindowThreadExit(void *arg);
+
 static int
  winRedirectErrorHandler(Display * pDisplay, XErrorEvent * pErr);
 
@@ -683,6 +686,8 @@ winMultiWindowWMProc(void *pArg)
     WMProcArgPtr pProcArg = (WMProcArgPtr) pArg;
     WMInfoPtr pWMInfo = pProcArg->pWMInfo;
 
+    pthread_cleanup_push(&winMultiWindowThreadExit, NULL);
+
     /* Initialize the Window Manager */
     winInitMultiWindowWM(pWMInfo, pProcArg);
 
@@ -893,6 +898,9 @@ winMultiWindowWMProc(void *pArg)
 #if CYGMULTIWINDOW_DEBUG
     ErrorF("-winMultiWindowWMProc ()\n");
 #endif
+
+    pthread_cleanup_pop(0);
+
     return NULL;
 }
 
@@ -915,6 +923,8 @@ winMultiWindowXMsgProc(void *pArg)
     Atom atmWindowState, atmMotifWmHints, atmWindowType, atmNormalHints;
     int iReturn;
     XIconSize *xis;
+
+    pthread_cleanup_push(&winMultiWindowThreadExit, NULL);
 
     winDebug("winMultiWindowXMsgProc - Hello\n");
 
@@ -1223,7 +1233,7 @@ winMultiWindowXMsgProc(void *pArg)
     }
 
     XCloseDisplay(pProcArg->pDisplay);
-    pthread_exit(NULL);
+    pthread_cleanup_pop(0);
     return NULL;
 }
 
@@ -1522,6 +1532,17 @@ winMultiWindowXMsgProcIOErrorHandler(Display * pDisplay)
         g_winMultiWindowXMsgProcOldIOErrorHandler(pDisplay);
 
     return 0;
+}
+
+/*
+ * winMultiWindowThreadExit - Thread exit handler
+ */
+
+static void
+winMultiWindowThreadExit(void *arg)
+{
+    /* multiwindow client thread has exited, stop server as well */
+    kill(getpid(), SIGTERM);
 }
 
 /*
