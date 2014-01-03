308e34818612168e0314fb45c9ea97d7b2a3b3d1
Jon TURNEY <jon.turney@dronecode.org.uk>
2014-01-03 13:21:40 +0000

    Tidy-up of winmsg.h
    - winVMsg() has no uses, so remove
    - winMsgVerb() has only one use, with default verbosity, so remove
    - winMsg() is identical to LogMessage()
    - Put winDrvMsg() and winDrvMsgVerb() under XWIN_XF86CONFIG
    
    which makes it possible to include winmsg.h in wmutil/ files to prototype
    winDebug() and winError()

--- hw/xwin/winconfig.c
+++ hw/xwin/winconfig.c
@@ -264,8 +264,8 @@ winConfigKeyboard(DeviceIntPtr pDevice)
                 break;
             }
             g_winInfo.keyboard.rate = (kbd_speed > 0) ? kbd_speed : 1;
-            winMsgVerb(X_PROBED, 1, "Setting autorepeat to delay=%d, rate=%d\n",
-                       g_winInfo.keyboard.delay, g_winInfo.keyboard.rate);
+            winMsg(X_PROBED, "Setting autorepeat to delay=%d, rate=%d\n",
+                   g_winInfo.keyboard.delay, g_winInfo.keyboard.rate);
         }
     }
 
