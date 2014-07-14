392d4fc588941a605003ee59bf07ff14ddd250c1
Jon TURNEY <jon.turney@dronecode.org.uk>
2014-07-14 19:41:25 +0100

    Downgrade "forcing window to exist" message to debug
    It happens whenever a GLX client uses GL on a window before it's been mapped, so
    don't log it like an error.

--- hw/xwin/glx/winpriv.c
+++ hw/xwin/glx/winpriv.c
@@ -85,7 +85,7 @@ winGetWindowInfo(WindowPtr pWin)
             }
 
             if (pWinPriv->hWnd == NULL) {
-                ErrorF("winGetWindowInfo: forcing window to exist\n");
+                winDebug("winGetWindowInfo: forcing window to exist\n");
                 winCreateWindowsWindowHierarchy(pWin);
             }
 
