83ba11f7e30052e503221cf87355b18e818fb247
Jon TURNEY <jon.turney@dronecode.org.uk>
2015-04-28 12:57:15 +0100

    Fix -silent-dup-error
    Looks like this has been broken since 042c6d861f7bb7038ddcdd6b59766fd9094d0e52
    "Use ErrorFSigSafe from FatalError and it's friends" on 2013-04-30.
    
    Signed-off-by: Jon TURNEY <jon.turney@dronecode.org.uk>

--- hw/xwin/winerror.c
+++ hw/xwin/winerror.c
@@ -48,16 +48,6 @@ OsVendorVErrorF(const char *pszFormat, va_list va_args)
     pthread_mutex_lock(&s_pmPrinting);
 #endif
 
-    /* If we want to silence it,
-     * detect if we are going to abort due to duplication error */
-    if (g_fSilentDupError) {
-        if ((strcmp(pszFormat, "InitOutput - Duplicate invocation on display number: %s.  Exiting.\n") == 0)
-            || (strcmp(pszFormat, "Server is already active for display %s\n%s %s\n%s\n") == 0)
-            || (strcmp(pszFormat, "MakeAllCOTSServerListeners: server already running\n") == 0)) {
-            g_fSilentFatalError = TRUE;
-        }
-    }
-
     /* Print the error message to a log file, could be stderr */
     LogVWrite(0, pszFormat, va_args);
 
@@ -78,6 +68,16 @@ void
 OsVendorFatalError(const char *f, va_list args)
 {
     char errormsg[1024] = "";
+
+    /* If we want to silence it,
+     * detect if we are going to abort due to duplication error */
+    if (g_fSilentDupError) {
+        if ((strcmp(f, "InitOutput - Duplicate invocation on display number: %s.  Exiting.\n") == 0)
+            || (strcmp(f, "Server is already active for display %s\n%s %s\n%s\n") == 0)
+            || (strcmp(f, "MakeAllCOTSServerListeners: server already running\n") == 0)) {
+            g_fSilentFatalError = TRUE;
+        }
+    }
 
     /* Don't give duplicate warning if UseMsg was called */
     if (g_fSilentFatalError)
