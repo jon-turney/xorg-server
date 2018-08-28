7d0f997a4b8e1b62c9b82e9b10934a6c55a6eb48
Colin Harrison <colin@xming.myzen.co.uk>
2018-08-28 15:30:23 +0100

    hw/xwin: Respect -notrayicon option on taskbar restart

--- hw/xwin/winwndproc.c
+++ hw/xwin/winwndproc.c
@@ -1177,7 +1177,7 @@ winWindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
         break;
 
     default:
-        if (message == s_uTaskbarRestart) {
+        if ((message == s_uTaskbarRestart) && !s_pScreenInfo->fNoTrayIcon)  {
             winInitNotifyIcon(s_pScreenPriv);
         }
         break;
